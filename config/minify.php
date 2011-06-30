ad48e053780249438b81af888eeae0758f5affeb
Slawomir Jasinski <slav123@gmail.com>
2011-06-30 20:57:53 +1000

    first commit

---
+++ config/minify.php
@@ -0,0 +1,22 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  Img
+*
+* Author: Slawomir Jasinski
+*		  slav123@gmail.com
+*         @slavomirj
+*
+* Location: http://github.com/slav123/CodeIgniter-Img/
+*
+* Created:  07.02.2011
+*
+* Description:
+*
+*/
+
+$config['assets_dir'] = base_url() . '/assets';
+$config['css_dir'] = 'css';
+$config['js_dir'] = 'js';
+
+/* End of file ion_auth.php */
+/* Location: ./system/application/config/img.php */
