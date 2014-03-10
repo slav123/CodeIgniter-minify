33d871f39fb71fb1f69526251291358a6af457ef
slav123 <slav123@gmail.com>
2014-03-11 10:24:25 +1100

    example views and controllers to test app

--- application/config/minify.php
+++ application/config/minify.php
@@ -14,9 +14,9 @@
 *
 */
 
-$config['assets_dir'] = base_url() . '/assets';
-$config['css_dir'] = 'css';
-$config['js_dir'] = 'js';
+$config['assets_dir'] = 'assets';
+$config['css_dir'] = 'assets/css';
+$config['js_dir'] = 'assets/js';
 
-/* End of file minify.php */
-/* Location: ./application/config/minify.php */
+// End of file minify.php
+// Location: ./application/config/minify.php
---
+++ application/views/welcome_message.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+	<?php // add css files
+		$this->minify->css(array('browser-specific.css', 'style.css'));
+		echo $this->minify->deploy_css(true);
+	?>
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to CodeIgniter!</h1>
+
+	<div id="body">
+		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>application/views/welcome_message.php</code>
+
+		<p>The corresponding controller for this page is found at:</p>
+		<code>application/controllers/welcome.php</code>
+
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+</div>
+
+</body>
+</html>
