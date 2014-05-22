9998c9788bd4e97d2e54b9f7e32b9d34702926da
Slawomir Jasinski <slav123@gmail.com>
2014-05-23 09:27:51 +1000

    js_compression_engine choice

--- application/config/minify.php
+++ application/config/minify.php
@@ -1,22 +1,35 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 /**
-* Name:  Minify
-*
-* Author: Slawomir Jasinski
-*		  slav123@gmail.com
-*         @slavomirj
-*
-* Location: http://www.spidersoft.com.au/projects/codeigniter-minify/
-*
-* Created:  07.02.2011
-*
-* Description:
-*
-*/
+ * Minify config Class
+ *
+ * PHP Version 5.3
+ *
+ * @category  PHP
+ * @package   Controller
+ * @author    Slawomir Jasinski <slav123@gmail.com>
+ * @copyright 2014 All Rights Reserved SpiderSoft
+ * @license   Copyright 2013 All Rights Reserved SpiderSoft
+ * @link      http://www.spidersoft.com.au/projects/codeigniter-minify/
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+/**
+ * Minify config gile
+ *
+ * @category  PHP
+ * @package   Controller
+ * @author    Slawomir Jasinski <slav123@gmail.com>
+ * @copyright 2014 All Rights Reserved SpiderSoft
+ * @license   Copyright 2012 All Rights Reserved SpiderSoft
+ * @link      http://www.spidersoft.com.au/projects/codeigniter-minify/
+ */
 
 $config['assets_dir'] = 'assets';
 $config['css_dir'] = 'assets/css';
 $config['js_dir'] = 'assets/js';
+$config['js_compression_engine'] = 'minify'; // cssmin
 
 // End of file minify.php
 // Location: ./application/config/minify.php
