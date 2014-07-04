0631514e2c70cef19d94ded38d8ca90ac216c0c3
slav123 <w8c45acu>
2014-07-04 17:03:18 +1000

    description update

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -15,18 +15,12 @@
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Welcome to CodeIgniter Minify library!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>Check out if minified files are working for you.</p>
+		<p>Click here to get <a href="assets/script.min.js">JavaScript</a> file</p>
+		<p>Click here to check out <a href="assets/style.min.css">CSS</a> </p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
