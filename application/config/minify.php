a187d9ef0cba65b25c69d1a6d9c7d9760eb9cec2
michalsn <michal@sniatala.pl>
2016-12-29 12:37:07 +0100

    update config file

--- application/config/minify.php
+++ application/config/minify.php
@@ -26,19 +26,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @link      http://www.spidersoft.com.au/projects/codeigniter-minify/
  */
 
+// output path where the compiled files will be stored (default value: 'assets')
 $config['assets_dir'] = 'assets';
-$config['css_dir']    = 'assets/css';
+
+// optional - path where the compiled css files will be stored (default value: '' - for backward compatibility)
+$config['assets_dir_css'] = ''; 
+
+// optional - path where the compiled js files will be stored (default value: '' - for backward compatibility)
+$config['assets_dir_js'] = '';     
+
+// where to look for css files (default value: 'assets/css')
+$config['css_dir'] = 'assets/css';
+
+// where to look for js files (default value: 'assets/js')
 $config['js_dir'] = 'assets/js';
 
+// default file name for css (default value: 'style.css')
+$config['css_file'] = 'styles.css';
+
+// default file name for js (default value: 'scripts.js')
+$config['js_file'] = 'scripts.js';
+
+// use automatic file names (default value: 'FALSE')
+$config['auto_names'] = FALSE;
+
+// compress files or not (default value: 'TRUE')
+$config['compress'] = TRUE;
+
+// compression engine setting (default values: 'minify' and 'closurecompiler')
 $config['compression_engine'] = array(
 	'css' => 'minify', // minify || cssmin
-	'js'  => 'closurecompiler' // jsmin || closurecompiler || jsminplus
+	'js'  => 'closurecompiler' // closurecompiler || jsmin || jsminplus
 );
 
-
-// optimization level (can be "WHITESPACE_ONLY", "SIMPLE_OPTIMIZATIONS" or "ADVANCED_OPTIMIZATIONS")
+// when you use closurecompiler as compression engine you can choose compression level (default value: 'SIMPLE_OPTIMIZATIONS')
+// avaliable options: "WHITESPACE_ONLY", "SIMPLE_OPTIMIZATIONS" or "ADVANCED_OPTIMIZATIONS"
 $config['closurecompiler']['compilation_level'] = 'SIMPLE_OPTIMIZATIONS';
-
 
 
 // End of file minify.php
