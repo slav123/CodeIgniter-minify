ff9cf0119af9f74b7194b9e2ca5220919421a984
slav123 <slav123@gmail.com>
2016-12-30 09:10:54 +0100

    fixed tests

--- tests/MinifyTest.php
+++ tests/MinifyTest.php
@@ -14,6 +14,7 @@ class MinifyTest extends PHPUnit_Framework_TestCase {
 		include_once('application/libraries/Minify.php');
 	}
 
+	// test init test
 	public function testInit()
 	{
 
@@ -24,6 +25,7 @@ class MinifyTest extends PHPUnit_Framework_TestCase {
 		$this->assertTrue(is_object($this->minify), 'is object');
 	}
 
+	// check js compression
 	public function testJsCompress()
 	{
 		$this->minify = new Minify();
@@ -37,6 +39,7 @@ class MinifyTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($this->minify->js_file, 'ut.min.js', 'output js file name');
 	}
 
+	// test css compresion
 	public function testCssCompress()
 	{
 		$this->minify = new Minify();
@@ -50,6 +53,7 @@ class MinifyTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($this->minify->css_file, 'ut.min.css', 'output css file name');
 	}
 
+	// test js with auto names
 	public function testJsCompressWithAutoNames()
 	{
 		$this->minify = new Minify();
@@ -64,6 +68,7 @@ class MinifyTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($this->minify->js_file, '91e30b9b77dc616476b94acf4dbb25c1.min.js', 'output js auto file name');
 	}
 
+	// test css with auto names
 	public function testCssCompressWithAutoNames()
 	{
 		$this->minify = new Minify();
@@ -78,6 +83,7 @@ class MinifyTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($this->minify->css_file, '72ac8bfd7cb9dd0f9df9ef4aafe0c714.min.css', 'output css auto file name');
 	}
 
+	// test js add
 	public function testJsCompressWithAdd()
 	{
 		$this->minify = new Minify();
@@ -91,6 +97,7 @@ class MinifyTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($this->minify->js_file, 'scripts.min.js', 'output js default file name');
 	}
 
+	// tetst css with cadd
 	public function testCssCompressWithAdd()
 	{
 		$this->minify = new Minify();
@@ -103,6 +110,7 @@ class MinifyTest extends PHPUnit_Framework_TestCase {
 
 		$this->assertEquals($this->minify->css_file, 'styles.min.css', 'output css default file name');
 	}
+
 
 	public function testJsCompressWithIndividualAutoName()
 	{
@@ -130,7 +138,7 @@ class MinifyTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($this->minify->css_file, '72ac8bfd7cb9dd0f9df9ef4aafe0c714.min.css', 'output css auto file name');
 	}
 
-// feature/custom_folders_for_compiled_css_and_js
+	// feature/custom_folders_for_compiled_css_and_js
 	public function testCustomJsPathForAssets()
 	{
 		$this->minify = new Minify();
@@ -143,16 +151,19 @@ class MinifyTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('<script type="text/javascript" src="assets/js/91e30b9b77dc616476b94acf4dbb25c1.min.js"></script>'.PHP_EOL, $result, 'deploy js with custom save path');
 	}
 
+	//
 	public function testCustomCssPathForAssets()
 	{
 		$this->minify = new Minify();
 
 		$this->minify->assets_dir_css = 'assets/css';
-		$this->minify->css_dir = 'assets/css';
+		$this->minify->css_dir        = 'assets/css';
 		$this->minify->css(array('style.css'));
 
 		$result = $this->minify->deploy_css(TRUE, 'auto');
-		$this->assertEquals('<link href="assets/css/72ac8bfd7cb9dd0f9df9ef4aafe0c714.min.css" rel="stylesheet" type="text/css" />'.PHP_EOL, $result, 'deploy css with custom save path');
+		$this->assertEquals('<link href="assets/css/72ac8bfd7cb9dd0f9df9ef4aafe0c714.min.css" rel="stylesheet" type="text/css" />' . PHP_EOL, $result, 'deploy css with custom save path');
+	}
+
 
 	public function testCssCompressWithGroupNames()
 	{
