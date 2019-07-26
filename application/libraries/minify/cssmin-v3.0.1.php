a2e7e99aaa595675f9fe352351bd0ee591f37f00
michalsn <michal@sniatala.pl>
2019-07-26 15:25:04 +0200

    replace deprecated each function

--- application/libraries/minify/cssmin-v3.0.1.php
+++ application/libraries/minify/cssmin-v3.0.1.php
@@ -2219,7 +2219,8 @@ class CssMin
 		{
 		// Create the class index for autoloading or including
 		$paths = array(dirname(__FILE__));
-		while (list($i, $path) = each($paths))
+		foreach($paths as $i => $path)
+		//while (list($i, $path) = each($paths))
 			{
 			$subDirectorys = glob($path . "*", GLOB_MARK | GLOB_ONLYDIR | GLOB_NOSORT);
 			if (is_array($subDirectorys))
